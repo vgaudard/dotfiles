59b5fbf8623685c68e0c1d1c94a9316c93cf43b6
Victor Gaudard <victor.gaudard@gmail.com>
2016-03-16 14:58:24 +0100

    Strict prompt to replace existing file

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@ declare -a SYMLINKS
 SYMLINKS=(".bashrc .bash_aliases .vimrc .inputrc")
 
 for f in $SYMLINKS; do
-    if [ ! -f "$f" ] || [ -L "$HOME/$f" ] || [ ! -f "$HOME/$f" ] || diff "$f" "$HOME/$f" > /dev/null ; then
+    if [ ! -f "$f" ] || [ ! -f "$HOME/$f" ]; then
         rm "$HOME/$f"
         ln -s "$(realpath $f)" "$HOME/$f"
     else
         #dialog --yesno "$f is not the same in current folder $(pwd -P)) and in $HOME\nDo you want to replace the current $f in $HOME with a symlink to $(realpath $f) ? (y/N)" 20 60
         #if [ "$?" -eq 0 ] ; then
-        echo "$f is not the same in current folder $(pwd -P)) and in $HOME"
+        echo "$f already exists in $HOME"
         echo "Do you want to replace the current $f in $HOME with a symlink to $(realpath $f) ? (y/N)"
         read ans
         case $ans in
