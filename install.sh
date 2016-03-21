de44de370da9813dbb8f4937ba320bdbfaa17b4a
Victor Gaudard <victor.gaudard@gmail.com>
2016-03-21 20:19:31 +0100

    Simpler install script

--- install.sh
+++ install.sh
@@ -4,24 +4,24 @@ declare -a SYMLINKS
 SYMLINKS=(".bashrc .bash_aliases .vimrc .inputrc")
 
 for f in $SYMLINKS; do
-    if [ ! -f "$f" ] || [ ! -f "$HOME/$f" ]; then
-        rm "$HOME/$f"
+    if [ ! -f "$HOME/$f" ]; then
         ln -s "$(realpath $f)" "$HOME/$f"
     else
-        #dialog --yesno "$f is not the same in current folder $(pwd -P)) and in $HOME\nDo you want to replace the current $f in $HOME with a symlink to $(realpath $f) ? (y/N)" 20 60
-        #if [ "$?" -eq 0 ] ; then
         echo "$f already exists in $HOME"
         echo "Do you want to replace the current $f in $HOME with a symlink to $(realpath $f) ? (y/N)"
+        if diff "$f" "$HOME/$f" > /dev/null ; then
+            echo "(Files have the same content)"
+        else
+            echo "(Files do not have the same content)"
+        fi
         read ans
         case $ans in
             y|Y)
-
-                echo "symlinking"
                 rm "$HOME/$f"
                 ln -s "$(realpath $f)" "$HOME/$f"
                 ;;
             *)
-                echo "Ok"
+                ;;
         esac
     fi
 done
