a1176dcbf5d6375301c6c9020b21f8f87c28bfb0
Victor Gaudard <victor.gaudard@gmail.com>
2016-05-16 22:33:43 +0200

    Added precious, kinda set up awesome

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ SYMLINKS=(["bash/.bashrc"]=""
           ["awesome/rc.lua"]="$HOME/.config/awesome"
           ["awesome/lua_completion.lua"]="$HOME/.config/awesome"
           ["awesome/lua_usefuleval.lua"]="$HOME/.config/awesome"
+          ["awesome/battery-warning.lua"]="$HOME/.config/awesome"
           ["awesome/precious"]="$HOME/.config/awesome")
 
 FOLDERS=("$HOME/.vim/backup"
